5f5af8829595c1de1b9dc40358a3e4b67cf9fbcc
Awais Ahmad <itxawais211@gmail.com>
2024-05-14 18:41:54 -0700

    in this part i have learnt all the javascript important concepts  that are essential for backend development

---
+++ script.js
@@ -0,0 +1,22 @@
+// let arr = [1,2,2,3,4,2] ;
+
+// let newArr = arr.filter((val)=>{
+//     if(val === 2 ) {return true} 
+//     else {return false}
+// })
+
+// console.log(newArr)
+
+
+// let obj = {
+//     name : "Awais",
+//     age : 18 
+// }
+// Object.freeze(obj)
+// obj.name = "Usman";
+// console.log(obj.name)
+
+function abcd(a,b){
+
+}
+console.log(abcd.length)
